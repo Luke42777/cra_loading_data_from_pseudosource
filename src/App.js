d80fbcb2791d8e9212a16f59491f9bb4ac46567b
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-02-15 16:46:31 +0000

    app works but still warnings to resolve and unmount component

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { Component } from 'react'
+import './App.css';
+
+const data = [
+  {
+    id: 1,
+    title: "Message nr 1",
+    content: "Message content no 1..."
+  },
+  {
+    id: 2,
+    title: "Message nr 2",
+    content: "Message content no 2..."
+  }
+]
+
+setInterval(() => {
+  let index = data.length + 1;
+  data.push({
+    key: index,
+    title: `Message nr ${index}`,
+    content: `Message content no ${index}...`
+  })
+}, 8000)
+
+class App extends Component {
+
+  state = {
+    comments: [...data],
+  }
+
+  getData = () => {
+    const newComments = [...data];
+    if (newComments.length !== this.state.comments.length) {
+      this.setState({
+        comments: newComments,
+      })
+    }
+  }
+
+  componentDidMount() {
+    setInterval(this.getData, 5000)
+  }
+  render() {
+    const comments = this.state.comments.map(comment => (
+      <div key={comment.id}>
+        <h3>{comment.title}</h3>
+        <div>{comment.content}</div>
+      </div>))
+    return (
+      <div className="App">{comments.reverse()}</div>
+    );
+  }
+}
+
+export default App;
